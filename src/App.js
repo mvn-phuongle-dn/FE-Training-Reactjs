8e8e413158d2f942c3b34722ed91e131174dc147
Phuong Le T.K <phuong.le@monstar-lab.com>
2021-06-11 09:23:13 +0700

    props state lifecycle

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
-import { Header } from './components/Header';
-import { Main } from './components/Main';
-import { Footer } from './components/Footer';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import logo from '../asets/logo.svg';
+import React, { Component } from 'react';
+import Circle from './Circle';
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 'home'
+    }
+  }
 
-export function Main() {
-  return (
-    <main className="page-main">
-      <div className="container">
-        <div className="main-wrap">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="title-app">
-            Hello React!
-          </h1>
+  handleChangePage(page){
+    this.setState({page : page})
+  }
+
+  render() {
+  const { page }  = this.state;
+    return (
+      <main className="page-main flex-center-all">
+        <div className="container">
+          <div className="main-wrap flex-center-all flex-column">
+            <div className="btn-wrap">
+              <button className="mr-15" onClick={()=>this.handleChangePage('home')}>HOME</button>
+              <button onClick={()=>this.handleChangePage('about')}>ABOUT</button>
+            </div>
+            {(page==='home') && 
+              <ul className="col-3 list-circle">
+                <Circle size={50} number={50} />
+                <Circle size={40} number={40} />
+                <Circle size={30} number={30} />
+              </ul>
+            }
+            {(page==='about') && <div>This is About page</div>}
+          </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  }
 }
+
+export default Main;
