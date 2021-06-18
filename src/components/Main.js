d9816ddc254681a2869a7935ed4ca2440a52922c
Phuong Le T.K <phuong.le@monstar-lab.com>
2021-06-18 17:23:19 +0700

    ex-day-3 list key action

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,62 @@
 import React, { Component } from 'react';
-import Circle from './Circle';
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      page: 'home'
+      notices: [{
+        id: 1,
+        color: 'red',
+        tittle: 'This is tittle 1',
+        des: 'Lorem ipsum dolor sit amet, consectetur adipisicing.'
+      }, {
+        id: 2,
+        color: 'yellow',
+        tittle: 'This is tittle 2',
+        des: 'Lorem ipsum dolor sit amet, consectetur adipisicing.'
+      }, {
+        id: 3,
+        color: 'green',
+        tittle: 'This is tittle 3',
+        des: 'Lorem ipsum dolor sit amet, consectetur adipisicing.'
+      }, {
+        id: 4,
+        color: 'brown',
+        tittle: 'This is tittle 4',
+        des: 'Lorem ipsum dolor sit amet, consectetur adipisicing.'
+      }, {
+        id: 5,
+        color: 'blue',
+        tittle: 'This is tittle 5',
+        des: 'Lorem ipsum dolor sit amet, consectetur adipisicing.'
+      }]
     }
   }
 
-  handleChangePage(page){
-    this.setState({page : page})
+  handleDeleteNotice(id) {
+    this.setState(prev => ({ notices: prev.notices.filter(item => item.id !== id) }))
   }
 
   render() {
-  const { page }  = this.state;
+    const { notices }  = this.state;
+    const renderNotices = 
+      <ul className="list-column">
+        {notices.map(item => (
+          <li key={item.id} className="item">
+            <div className="notice-box mr-15" style={{backgroundColor: item.color}}></div>
+            <div className="text-left mr-15">
+              <h3 className="mb-8">{item.tittle}</h3>
+              <p className="fs-14 txt-gray">{item.des}</p>
+            </div>
+            <i class="fas fa-times delete" onClick={()=>{this.handleDeleteNotice(item.id)}}></i>
+          </li>
+          ))}
+      </ul>
     return (
       <main className="page-main flex-center-all">
         <div className="container">
           <div className="main-wrap flex-center-all flex-column">
-            <div className="btn-wrap">
-              <button className="mr-15" onClick={()=>this.handleChangePage('home')}>HOME</button>
-              <button onClick={()=>this.handleChangePage('about')}>ABOUT</button>
+            { notices.length === 0 ? <p className="fs-18">You don't have any notice.</p> : renderNotices }
             </div>
-            {(page==='home') && 
-              <ul className="col-3 list-circle">
-                <Circle size={50} number={50} />
-                <Circle size={40} number={40} />
-                <Circle size={30} number={30} />
-              </ul>
-            }
-            {(page==='about') && <div>This is About page</div>}
-          </div>
         </div>
       </main>
     );
